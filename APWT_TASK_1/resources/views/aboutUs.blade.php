73a53920379e1adbfc9335c677e28f8aabbc9d6c
RiyazHossan <riyazhossanrobin@gmail.com>
2022-03-11 10:51:14 +0600

    TASK_1

---
+++ APWT_TASK_1/resources/views/aboutUs.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+@section('content')
+    <h1>Advanced Programming in Web Technology</h1>
+    <h2>Section:C</h2>
+    <h2>Group - 7 </h2>
+@endsection
---
+++ APWT_TASK_1/resources/views/inc/navBar.blade.php
@@ -0,0 +1,6 @@
+<a class = "btn btn-primary" href="{{route('home')}}">Home</a>
+<a class = "btn btn-primary" href="{{route('product')}}">Products</a>
+<a class = "btn btn-primary" href="{{route('ourTeam')}}">Our Team</a>
+<a class = "btn btn-primary" href="{{route('aboutUs')}}">About Us</a>
+<a class = "btn btn-primary" href="{{route('contactUs')}}">Contact Us</a>
+
